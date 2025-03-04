1afe4f615e5aeb0b272c695b9a1c67a508c92582
JeremyJFarnsworth <jdog1123@gmail.com>
2025-03-04 09:25:38 -0700

    user creation without ui

--- FinaceTrackerApp/src/edu/neumont/mgt101/model/User.java
+++ FinaceTrackerApp/src/edu/neumont/mgt101/model/User.java
@@ -1,4 +1,65 @@
 package edu.neumont.mgt101.model;
 
+import java.io.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class User {
+    private static final String DATABASE_FILE = "database.txt";
+
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = hashPassword(password);
+    }
+
+    // Saves user credentials to database.txt
+    public boolean registerUser() {
+        if (isUsernameTaken(username)) {
+            System.out.println("Username already exists.");
+            return false;
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(DATABASE_FILE, true))) {
+            writer.write(username + "," + password);
+            writer.newLine();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    // Checks if the username is already taken
+    private boolean isUsernameTaken(String username) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(DATABASE_FILE))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] userData = line.split(",");
+                if (userData.length > 0 && userData[0].equals(username)) {
+                    return true; // Username already exists
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    // Hashes a password using SHA-256
+    private static String hashPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(password.getBytes());
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hash) {
+                hexString.append(String.format("%02x", b));
+            }
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Error hashing password", e);
+        }
+    }
 }
--- FinaceTrackerApp/src/edu/neumont/mgt101/view/FinanceUI.java
+++ FinaceTrackerApp/src/edu/neumont/mgt101/view/FinanceUI.java
@@ -2,4 +2,16 @@ package edu.neumont.mgt101.view;
 
 public class FinanceUI {
 
+    public static int displayMainMenu() {
+        return Console.getIntInput("""
+                1. Create Account
+                2. Login
+                3. Exit
+                """, 1, 5);
+    }
+    public static int displayLoggedInMenu() {
+        return Console.getIntInput("""
+                1. 
+                """, 1, 5);
+    }
 }
