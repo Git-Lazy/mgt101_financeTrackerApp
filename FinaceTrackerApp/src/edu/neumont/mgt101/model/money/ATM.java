152d6de7f57faf138a6c3a0db842dbad20b16cbd
SamuelE <wolfzilla200@gmail.com>
2025-03-05 09:31:21 -0700

    Added the beginings of tracking transactions

--- FinaceTrackerApp/src/edu/neumont/mgt101/model/money/ATM.java
+++ FinaceTrackerApp/src/edu/neumont/mgt101/model/money/ATM.java
@@ -1,6 +1,16 @@
 package edu.neumont.mgt101.model.money;
 
+import java.util.ArrayList;
+
 public class ATM {
     // the go between from the balance and the user (because the user can't just set how much money they have
     // can be taken out if it seems to be redundant
+    //TODO: check transaction history; use an ArrayList
+    private ArrayList<Integer> balanceTransactions = new ArrayList<>();
+    private Balance userBalance = new Balance();
+
+    public ATM() {
+        // wait for login branch
+
+    }
 }
